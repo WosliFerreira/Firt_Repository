d5917561f2e59377227e2de679e6f58930ec586d
Wosli Ferreira <woslyc@gmail.com>
2022-09-11 21:37:10 -0300

    a

--- Python_Study/Aulas/Python_Intermediario/Funcao_def_1.py
+++ Python_Study/Aulas/Python_Intermediario/Funcao_def_1.py
@@ -1,12 +1,22 @@
 """
 Funções - def(Parte 1)
 """
+# def saudacao(msg='Ola', nome='Usuario'):
+#   nome = nome.replace('e', '3')
+#   msg = msg.replace('e', '3')
+#   return f'{msg} {nome}'
+# variavel = saudacao()
+# print(variavel)
 
-def saudacao(msg='ola',nome='usuario'):
-   nome = nome.replace('e', '3')
-   msg = msg.replace('e', '3')
-   return f'{msg} {nome}'
+def divisao(n1, n2):
+   if n2 == 0:
+      return
 
-variavel = saudacao()
+   return n1 / n2
 
-print(variavel)
+divide = divisao(8,4)
+
+if divide:
+   print(divide)
+else:
+   print('Conta Invalida')
